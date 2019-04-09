f9a7a64e7a35ad85cff806e6e4e6d14f7300fca5
Powerlated <zjia08@gmail.com>
2019-04-09 17:46:16 -0400

    Got basic stats set & get working

--- src/index.js
+++ src/index.js
@@ -1,14 +1,88 @@
-var restify = require('restify');
+const restify = require("restify");
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+const logger = require("winston");
+
+logger.add(new logger.transports.Console());
+
+db.defaults({ msgs: [], scores: {} }).write();
+
+function respondScorePlayer(req, res, next) {
+  let stats = db.get("scores").value();
+
+  let reqName = req.params.name;
+
+  let returnScore;
+  let returnStatus = 200;
+
+  let err;
+
+  console.log("SCORE: Asked for score of a player: " + reqName);
+
+  if (stats[reqName]) {
+    console.log(`[200] Score for player ${reqName}: ${stats[reqName]}`);
+    returnScore = stats[reqName];
+  } else {
+    err = `[404] Score for player ${reqName} not found.`;
+    console.log(err);
+    returnStatus = 404;
+    returnScore = 0;
+  }
+
+  res.send(returnStatus, { score: returnScore, err: err });
   next();
 }
 
-var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+function respondScores(req, res, next) {
+  let stats = db.get("scores").value();
+  console.log("SCORE: Asked for all scores");
+  console.log(`[200] Scores: ${stats}`);
+  res.send(200, stats);
+  next();
+}
 
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+function setScorePlayer(req, res, next) {
+  let data = req.body;
+
+  let name = req.params.name;
+
+  if (parseInt(data) || !data) {
+    db.get("scores")
+      .set(name, data)
+      .write();
+
+    res.send(200);
+  } else {
+    res.send(422);
+  }
+
+  next();
+}
+
+var server = restify.createServer({ name: "TKPass-Server" });
+
+// JSON Stuff
+server.use(restify.plugins.jsonBodyParser({ mapParams: true }));
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser({ mapParams: true }));
+server.use(restify.plugins.fullResponse());
+
+server.pre(restify.pre.sanitizePath());
+server.use(restify.plugins.queryParser());
+
+// server.get("/hello/:name", respond);
+// server.head("/hello/:name", respond);
+
+server.get("/api/stats/:name", respondScorePlayer);
+server.get("/api/stats", respondScores);
+
+server.post("/api/stats/:name", setScorePlayer);
+
+server.listen(8081, function() {
+  console.log("%s listening at %s", server.name, server.url);
 });
